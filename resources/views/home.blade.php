a560392a00b773f0ac86ec5516c51b0900ac1f88
bryanvalerian03 <bryan.valerian23@gmail.com>
2022-07-06 21:50:24 +0700

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
 @section('title', 'Home Page')
 
 @section('container')
-    @include('partials.navbar')
+    {{-- @include('partials.navbar')
+    <img src="\storage\Banner 1.png" alt="" width="100%"> --}}
     @foreach ($games as $game)
         <img src="{{ asset('storage/' . $game->game_img) }}" alt="" width="150">
     @endforeach
