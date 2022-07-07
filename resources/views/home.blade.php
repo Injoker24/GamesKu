2827d5ff2557c7de89c2e82d4be2bac62c3e1bd4
Injoker24 <mcl2411@gmail.com>
2022-07-07 19:39:32 +0700

    update seeder, update navbar, created css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
 @section('container')
     @include('partials.navbar')
     <img src="\storage\Banner 1.png" alt="" width="100%">
+    
     @foreach ($games as $game)
         <img src="{{ asset('storage/' . $game->game_img) }}" alt="" width="150">
     @endforeach
