5849197694a6ac20f3671672cd15070f6130c9d6
Injoker24 <mcl2411@gmail.com>
2022-07-08 10:58:35 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,23 +4,30 @@
 
 @section('container')
     @include('partials.navbar')
+    <div class="banner-container">
+        <h1>BINGUNG CARI TEMPAT BELI SKIN?</h1>
+        <h1><span style="color:var(--accent)">GAMESKU-IN</span> AJA</h1>
+    </div>
 
-    <img src="\storage\Banner 1.png" alt="" width="100%" height="400">
     <div class="container mt-5">
+        <div class="main-search-input fl-wrap mb-5">
+            <div class="main-search-input-item">
+                <input type="text" value="" placeholder="Search Games...">
+            </div>
+            <button class="main-search-button">SEARCH</button>
+        </div>
+        <h1>ALL GAMES</h1>
         @foreach ($games as $game)
-            <div class="card bg-dark">
-                <img src="{{ asset('storage/' . $game->game_img) }}" class="card-img" alt="..." height="100" style="object-fit:cover; object-position:cover; filter:brightness(50%)">
+            <div class="card bg-dark mb-4 games-card">
+                <img src="{{ asset('storage/' . $game->game_img) }}" alt="..." height="150" style="object-fit:cover; object-position:cover; filter:brightness(50%)">
                 <div class="card-img-overlay">
-                    <div class="d-flex">
-                        <img src="{{ asset('storage/' . $game->game_logo) }}" alt="" width="50">
-                        <div class="info ms-3" style="color: #66FCF1">
-                            <h5 class="card-title">{{ $game->name }}</h5>
-                            <p class="card-text">{{ $game->developer }}</p>
-                        </div>
+                    <img src="{{ asset('storage/' . $game->game_logo) }}" alt="" width="75" height="75" style="object-fit:cover; border-radius:10px">
+                    <div class="info ms-3" style="color: var(--accent)">
+                        <h5 class="card-title">{{ $game->name }}</h5>
+                        <p class="card-text">{{ $game->developer }}</p>
                     </div>
                 </div>
             </div>
-            {{-- <img src="{{ asset('storage/' . $game->game_img) }}" alt="" width="150"> --}}
         @endforeach
     </div>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,14 +1,14 @@
 <div class="navbar">
-    <a class="navbar-brand" href="#" style="padding-left: 50px;"><img src="{{ asset('storage/GamesKu Icon.png') }}" alt="" width="75"></a>
+    <a href="#" style="padding-left: 50px;"><img src="{{ asset('storage/GamesKu Logo.png') }}" alt="" width="200"></a>
     <div style="padding-left: 100px;">
         <a class="nav-details" href="/">HOME</a>
         @auth
             <a class="nav-details" href="#">TRANSACTION</a>
             <a class="nav-details" href="#">HISTORY</a>
-            <a class ="nav-details" href="/logout">LOGOUT</a>
+            <a class="nav-details" href="/logout">LOGOUT</a>
         @else
             <a class="nav-details" href="/register">REGISTER</a>
-            <a class ="nav-details" href="/login">LOGIN</a>
+            <a class="nav-details" href="/login">LOGIN</a>
         @endauth
     </div>
 </div>
