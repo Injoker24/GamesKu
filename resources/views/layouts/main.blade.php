4d2a027974a097f33bcecd936f59d9c2c3a71a63
Injoker24 <mcl2411@gmail.com>
2022-07-08 12:41:07 +0700

    preloader

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link href="/css/style.css" rel="stylesheet">
+    <link href="/css/preloader.css" rel="stylesheet">
     <title>@yield('title')</title>
   </head>
 </head>
@@ -42,7 +43,12 @@
         {{-- @include('partials.navbar') --}}
     {{-- @endif --}}
     {{-- perlu navbar tinggal include di masing" section --}}
+    <div class="loader">
+        <div class="loading-dot"></div>
+    </div>
     @yield('container')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="/js/script.js"></script>
 </body>
 </html>
