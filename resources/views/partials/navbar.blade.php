074436a3e9e1273b61f0672a6214bbd3850c44ad
bryanvalerian03 <bryan.valerian23@gmail.com>
2022-07-08 18:42:39 +0700

    admin

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,8 +3,13 @@
     <div style="padding-left: 100px;">
         <a class="nav-details" href="/">HOME</a>
         @auth
-            <a class="nav-details" href="#">TRANSACTION</a>
-            <a class="nav-details" href="#">HISTORY</a>
+            @if (Auth::user()->IsAdmin)
+                <a class="nav-details" href="#">MANAGE TRANSACTIONS</a>
+                <a class="nav-details" href="#">MANAGE GAME</a>
+            @else
+                <a class="nav-details" href="#">TRANSACTION</a>
+                <a class="nav-details" href="#">HISTORY</a>
+            @endif
             <a class="nav-details" href="/logout">LOGOUT</a>
         @else
             <a class="nav-details" href="/register">REGISTER</a>
