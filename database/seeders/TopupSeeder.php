a3d4f83a7c54fb24f704f6b04af834a67fb69e07
Turic <ivanderglad200@gmail.com>
2022-07-10 22:43:59 +0700

    update top up database

--- database/seeders/TopupSeeder.php
+++ database/seeders/TopupSeeder.php
@@ -16,205 +16,464 @@ class TopupSeeder extends Seeder
     {
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
             'amount' => '3',
             'price' => 1680
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
             'amount' => '5',
-            'price' => 1579
+            'price' => 1665
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
             'amount' => '12',
             'price' => 3688
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
             'amount' => '19',
             'price' => 5797
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
             'amount' => '28',
             'price' => 8436
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
+            'amount' => '44',
+            'price' => 13320
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
             'amount' => '59',
             'price' => 17760
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
+            'amount' => '85',
+            'price' => 25530
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
             'amount' => '170',
             'price' => 51060
         ]);
 
         Topup::create([
             'game_id' => '1',
-            'topup_type' => 'diamond',
+            'topup_type' => 'Diamond',
+            'amount' => '240',
+            'price' => 68543
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
             'amount' => '296',
             'price' => 88800
         ]);
 
         Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
+            'amount' => '408',
+            'price' => 122100
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
+            'amount' => '568',
+            'price' => 166500
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
+            'amount' => '875',
+            'price' => 255300
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
+            'amount' => '2010',
+            'price' => 555000
+        ]);
+
+        Topup::create([
+            'game_id' => '1',
+            'topup_type' => 'Diamond',
+            'amount' => '4830',
+            'price' => 1332000
+        ]);
+
+        Topup::create([
             'game_id' => '2',
-            'topup_type' => 'points',
+            'topup_type' => 'Points',
             'amount' => '125',
             'price' => 15000
         ]);
 
         Topup::create([
             'game_id' => '2',
-            'topup_type' => 'points',
+            'topup_type' => 'Points',
             'amount' => '420',
             'price' => 50000
         ]);
 
         Topup::create([
             'game_id' => '2',
-            'topup_type' => 'points',
+            'topup_type' => 'Points',
             'amount' => '700',
             'price' => 80000
         ]);
 
         Topup::create([
             'game_id' => '2',
-            'topup_type' => 'points',
+            'topup_type' => 'Points',
             'amount' => '1375',
             'price' => 150000
         ]);
 
         Topup::create([
             'game_id' => '2',
-            'topup_type' => 'points',
+            'topup_type' => 'Points',
             'amount' => '2400',
             'price' => 250000
         ]);
 
         Topup::create([
+            'game_id' => '2',
+            'topup_type' => 'Points',
+            'amount' => '4000',
+            'price' => 400000
+        ]);
+
+        Topup::create([
+            'game_id' => '2',
+            'topup_type' => 'Points',
+            'amount' => '8150',
+            'price' => 800000
+        ]);
+
+        Topup::create([
             'game_id' => '3',
-            'topup_type' => 'vip',
+            'topup_type' => 'Vip',
             'amount' => '25',
             'price' => 5000
         ]);
 
         Topup::create([
             'game_id' => '3',
-            'topup_type' => 'vip',
+            'topup_type' => 'Vip',
+            'amount' => '36',
+            'price' => 7000
+        ]);
+
+        Topup::create([
+            'game_id' => '3',
+            'topup_type' => 'Vip',
             'amount' => '52',
             'price' => 10000
         ]);
 
         Topup::create([
             'game_id' => '3',
-            'topup_type' => 'vip',
+            'topup_type' => 'Vip',
+            'amount' => '73',
+            'price' => 14000
+        ]);
+
+        Topup::create([
+            'game_id' => '3',
+            'topup_type' => 'Vip',
             'amount' => '131',
             'price' => 25000
         ]);
 
         Topup::create([
             'game_id' => '3',
-            'topup_type' => 'vip',
+            'topup_type' => 'Vip',
+            'amount' => '263',
+            'price' => 50000
+        ]);
+
+        Topup::create([
+            'game_id' => '3',
+            'topup_type' => 'Vip',
             'amount' => '530',
             'price' => 100000
         ]);
 
         Topup::create([
             'game_id' => '3',
-            'topup_type' => 'vip',
+            'topup_type' => 'Vip',
             'amount' => '1375',
             'price' => 250000
         ]);
 
         Topup::create([
+            'game_id' => '3',
+            'topup_type' => 'Vip',
+            'amount' => '2875',
+            'price' => 500000
+        ]);
+
+        Topup::create([
+            'game_id' => '3',
+            'topup_type' => 'Vip',
+            'amount' => '6000',
+            'price' => 1000000
+        ]);
+
+        Topup::create([
             'game_id' => '4',
-            'topup_type' => 'Syndicate Gold Pack 1',
+            'topup_type' => 'Syndicate Gold Pack 1 (90 Gold)',
             'amount' => '90',
             'price' => 16000
         ]);
 
         Topup::create([
             'game_id' => '4',
-            'topup_type' => 'Syndicate Gold Pack 2',
+            'topup_type' => 'Syndicate Gold Pack 2 (280 Gold)',
             'amount' => '280',
             'price' => 35000
         ]);
 
         Topup::create([
             'game_id' => '4',
-            'topup_type' => 'Syndicate Gold Pack 4',
+            'topup_type' => 'Syndicate Gold Pack 4 (500 Gold)',
             'amount' => '500',
             'price' => 65000
         ]);
 
         Topup::create([
             'game_id' => '4',
-            'topup_type' => 'Syndicate Gold Pack 6',
+            'topup_type' => 'Syndicate Gold Pack 6 (1050 Gold)',
             'amount' => '1050',
             'price' => 129000
         ]);
 
         Topup::create([
             'game_id' => '4',
-            'topup_type' => 'Syndicate Gold Pack 8',
+            'topup_type' => 'Syndicate Gold Pack 8 (2150 Gold)',
             'amount' => '2150',
             'price' => 259000
         ]);
 
         Topup::create([
+            'game_id' => '4',
+            'topup_type' => 'Syndicate Gold Pack 10 (2750 Gold)',
+            'amount' => '2750',
+            'price' => 309000
+        ]);
+
+        Topup::create([
+            'game_id' => '4',
+            'topup_type' => 'Syndicate Gold Pack 16 (5650 Gold)',
+            'amount' => '5650',
+            'price' => 629000
+        ]);
+
+        Topup::create([
+            'game_id' => '4',
+            'topup_type' => 'Syndicate Gold Pack 19 (11500 Gold)',
+            'amount' => '11500',
+            'price' => 1249000
+        ]);
+
+        Topup::create([
+            'game_id' => '4',
+            'topup_type' => 'Syndicate Gold Pack 22 (23500 Gold)',
+            'amount' => '23500',
+            'price' => 2499000
+        ]);
+
+        Topup::create([
+            'game_id' => '5',
+            'topup_type' => 'Show-Bucks',
+            'amount' => '1000',
+            'price' => 80000
+        ]);
+
+        Topup::create([
+            'game_id' => '5',
+            'topup_type' => 'Show-Bucks',
+            'amount' => '2800',
+            'price' => 200000
+        ]);
+
+        Topup::create([
+            'game_id' => '5',
+            'topup_type' => 'Show-Bucks',
+            'amount' => '5000',
+            'price' => 320000
+        ]);
+
+        Topup::create([
+            'game_id' => '5',
+            'topup_type' => 'Show-Bucks',
+            'amount' => '13500',
+            'price' => 800000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Gems',
+            'amount' => '500',
+            'price' => 60000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Gems',
+            'amount' => '1200',
+            'price' => 120000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Gems',
+            'amount' => '1700',
+            'price' => 180000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Gems',
+            'amount' => '2500',
+            'price' => 240000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Gems',
+            'amount' => '3000',
+            'price' => 300000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Gems',
+            'amount' => '3700',
+            'price' => 360000
+        ]);
+
+        Topup::create([
+            'game_id' => '6',
+            'topup_type' => 'Royale Pass',
+            'amount' => '1',
+            'price' => 60000
+        ]);
+
+        Topup::create([
             'game_id' => '7',
-            'topup_type' => 'diamonds',
+            'topup_type' => 'Diamonds',
             'amount' => '5',
             'price' => 1000
         ]);
 
         Topup::create([
             'game_id' => '7',
-            'topup_type' => 'diamonds',
+            'topup_type' => 'Diamonds',
             'amount' => '12',
             'price' => 2000
         ]);
 
         Topup::create([
             'game_id' => '7',
-            'topup_type' => 'diamonds',
+            'topup_type' => 'Diamonds',
             'amount' => '50',
             'price' => 8000
         ]);
 
         Topup::create([
             'game_id' => '7',
-            'topup_type' => 'diamonds',
+            'topup_type' => 'Diamonds',
+            'amount' => '70',
+            'price' => 10000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
             'amount' => '140',
             'price' => 20000
         ]);
 
         Topup::create([
             'game_id' => '7',
-            'topup_type' => 'diamonds',
+            'topup_type' => 'Diamonds',
+            'amount' => '355',
+            'price' => 50000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
             'amount' => '720',
             'price' => 100000
         ]);
 
         Topup::create([
             'game_id' => '7',
-            'topup_type' => 'diamonds',
+            'topup_type' => 'Diamonds',
+            'amount' => '1450',
+            'price' => 200000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
             'amount' => '2180',
             'price' => 300000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
+            'amount' => '3640',
+            'price' => 500000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
+            'amount' => '7290',
+            'price' => 1000000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
+            'amount' => '36500',
+            'price' => 5000000
+        ]);
+
+        Topup::create([
+            'game_id' => '7',
+            'topup_type' => 'Diamonds',
+            'amount' => '73100',
+            'price' => 10000000
         ]);
 
         Topup::create([
