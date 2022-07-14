77b629e3a0b5676ca6831569b43612c0e2a2a6eb
Turic <ivanderglad200@gmail.com>
2022-07-14 17:59:56 +0700

    carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,45 @@
 
 @section('container')
     @include('partials.navbar')
-    <div class="banner-container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="banner-container">
+                <h1>BINGUNG CARI TEMPAT BELI SKIN?</h1>
+                <h1><span style="color:var(--accent)">GAMESKU-IN</span> AJA</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="banner-container">
+                <h1>BINGUNG CARI TEMPAT BELI SKIN?</h1>
+                <h1><span style="color:var(--accent)">GAMESKU-IN</span> AJA</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="banner-container">
+                <h1>BINGUNG CARI TEMPAT BELI SKIN?</h1>
+                <h1><span style="color:var(--accent)">GAMESKU-IN</span> AJA</h1>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    {{-- <div class="banner-container">
         <h1>BINGUNG CARI TEMPAT BELI SKIN?</h1>
         <h1><span style="color:var(--accent)">GAMESKU-IN</span> AJA</h1>
-    </div>
+    </div> --}}
 
     <div class="container mt-5">
         <div class="main-search-input fl-wrap mb-5">
