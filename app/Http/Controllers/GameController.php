dad0599c03596f15fc5f4aa778f3ceff2bcc495c
Turic <ivanderglad200@gmail.com>
2022-07-15 15:04:14 +0700

    update search

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -11,7 +11,7 @@ class GameController extends Controller
     public function searchGame(Request $request)
     {
         // dd($request->input('game'));
-        $game = Game::where('name', 'like', '%' . $request->input('game') . '%')->get();
+        $game = Game::where('name', 'like', '%' . $request->input('game') . '%')->orWhere('developer', 'like', '%' . $request->input('game') . '%')->get();
         return view('guest.search', [
             'games' => $game
         ]);
